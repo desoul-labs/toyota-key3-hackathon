4106d18b52e3fb6e390c5fc4ea7ab71bad0ebe3b
Lmmmmm <lm839377386@gmail.com>
2023-03-11 23:32:56 +0900

    feat: implement task

--- app/src/TaskItem.tsx
+++ app/src/TaskItem.tsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { Avatar, Box, Button, Divider, Input, List, ListItem, ListItemAvatar, ListItemText, Modal, Typography } from "@mui/material";
+import { Avatar, Box, Button, Divider, Input, List, ListItem, ListItemAvatar, ListItemText, Modal, Typography, OutlinedInput, InputAdornment, IconButton } from "@mui/material";
 import Person2Icon from '@mui/icons-material/Person2';
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm';
+import CloseIcon from '@mui/icons-material/Close';
 
 interface Props {
   item: {
-    name: string;
-    dueDate: string;
-    assignedTo: string;
-    status: 0 | 1 | 2 | 3 | 4 | 5;
+    id: string;
+    title: string;
+    description: string;
+    user?: string;
+    expiredAt: string;
+    status: 0 | 1 | 2;
   };
 }
 
@@ -42,7 +45,7 @@ function TaskItem({ item }: Props) {
         <ListItem alignItems="flex-start">
           <div className='flex justify-between w-full'>
             <div className="flex flex-col">
-              <h4 className='mb-2'>{item.name}</h4>
+              <h4 className='mb-2'>{item.title}</h4>
               <ListItemText
                 secondary={
                   <React.Fragment>
@@ -53,10 +56,10 @@ function TaskItem({ item }: Props) {
                       variant="body2"
                       color="text.primary"
                     >
-                      {item.assignedTo}
+                      {item.user}
                     </Typography>
                     <AccessAlarmIcon />
-                    {item.dueDate}
+                    {item.expiredAt}
                   </React.Fragment>
                 }
               />
@@ -64,7 +67,7 @@ function TaskItem({ item }: Props) {
             {(item.status > 0 && item.status <= 4) &&
               <Button
                 variant="outlined"
-                disabled={item.status === 4 || item.status === 5}
+                // disabled={item.status === 4 || item.status === 5}
                 className="bg-blue-500 text-white font-bold py-2 px-4 rounded h-10"
                 onClick={handleOpen}
               >
@@ -81,19 +84,78 @@ function TaskItem({ item }: Props) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <div className="fixed w-1/2 h-2/3 top-1/4 left-1/4 bg-white rounded-md shadow-lg p-8">
-          <h2 className="text-2xl font-bold mb-4 text-center">Modal Title</h2>
-          <div className="flex items-center mb-4">
-            <Person2Icon fontSize="large" className="text-gray-500 mr-2" />
-            <span className="text-lg font-bold">John Doe</span>
-            <span className="ml-4 text-gray-500">Due Date: 2023-03-01</span>
+        {/* <div className="fixed w-1/2 min-w-full h-2/3 top-1/4 left-1/4 bg-white rounded-md shadow-lg p-8"> */}
+        <div className="flex items-center justify-center h-full">
+          <div className="relative w-full sm:w-1/2 bg-white rounded-md shadow-lg p-8">
+            <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
+              <IconButton
+                aria-label="close"
+                onClick={handleClose}
+                className="absolute top-1 right-1"
+              >
+                <CloseIcon />
+              </IconButton>
+            </div>
+            <h2 className="text-2xl font-bold mb-4 text-center">Modal Title</h2>
+            <div className="flex items-center mb-4">
+              <Person2Icon fontSize="large" className="text-gray-500 mr-2" />
+              <span className="text-lg font-bold">John Doe</span>
+              <span className="ml-4 text-gray-500">Due Date: 2023-03-01</span>
+            </div>
+            <p className="text-lg text-gray-800">
+              This is the description of the modal.
+            </p>
+            <div className="flex flex-col mt-10">
+              <Typography>評価点(0-100)</Typography>
+              <OutlinedInput
+                id="outlined-adornment-weight"
+                size="small"
+                aria-describedby="outlined-weight-helper-text"
+                sx={{ mb: 2 }}
+              />
+              <Button variant="contained">評価</Button>
+            </div>
           </div>
-          <p className="text-lg text-gray-800">
-            This is the description of the modal.
-          </p>
-          <div className="flex flex-col">
-            <Input placeholder="Name" />
-            <Button variant="contained">Save</Button>
+        </div>
+      </Modal>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        {/* <div className="fixed w-1/2 min-w-full h-2/3 top-1/4 left-1/4 bg-white rounded-md shadow-lg p-8"> */}
+        <div className="flex items-center justify-center h-full">
+          <div className="relative w-full sm:w-1/2 bg-white rounded-md shadow-lg p-8">
+            <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
+              <IconButton
+                aria-label="close"
+                onClick={handleClose}
+                className="absolute top-1 right-1"
+              >
+                <CloseIcon />
+              </IconButton>
+            </div>
+            <h2 className="text-2xl font-bold mb-4 text-center">Modal Title</h2>
+            <div className="flex items-center mb-4">
+              <Person2Icon fontSize="large" className="text-gray-500 mr-2" />
+              <span className="text-lg font-bold">John Doe</span>
+              <span className="ml-4 text-gray-500">Due Date: 2023-03-01</span>
+            </div>
+            <p className="text-lg text-gray-800">
+              This is the description of the modal.
+            </p>
+            <div className="flex flex-col mt-10">
+              <Typography>評価点(0-100)</Typography>
+              <OutlinedInput
+                id="outlined-adornment-weight"
+                size="small"
+                aria-describedby="outlined-weight-helper-text"
+                sx={{ mb: 2 }}
+              />
+              <Button variant="contained">評価</Button>
+            </div>
           </div>
         </div>
       </Modal>
--- app/src/TaskList.tsx
+++ app/src/TaskList.tsx
@@ -1,13 +1,68 @@
+import { useEffect, useState } from "react";
 import TaskItem from "./TaskItem";
+import dayjs, { Dayjs } from 'dayjs';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import { Button, IconButton, Modal, OutlinedInput, TextField, Typography } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+import { LocalizationProvider, DatePicker } from '@mui/x-date-pickers';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+
 interface item {
-  name: string;
-  dueDate: string;
-  assignedTo: string;
-  status: 0 | 1 | 2 | 3 | 4 | 5;
+  id: string;
+  title: string;
+  description: string;
+  expiredAt: string;
+  user?: string;
+  status: 0 | 1 | 2;
 };
 
 function TaskList() {
+  const [tasks, setTasks] = useState<item[]>()
+  const [open, setOpen] = useState(false)
+  const [title, setTitle] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+  const [user, setUser] = useState<string>('')
+
+  const [timeValue, setTimeValue] = useState<Dayjs | null>();
+  const handleOpen = () => setOpen(true)
+  const handleClose = () => setOpen(false)
+  const TaskCreationClicked = () => {
+    setOpen(true)
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('https://toyota-hackathon.azurewebsites.net/api/GetTasks');
+      const data = await response.json();
+      console.log(data)
+      setTasks(data);
+    }
+    fetchData()
+  }, [])
+
+  const createTask = async () => {
+    const task: item = {
+      id: '4',
+      title: title,
+      description: description,
+      expiredAt: timeValue?.toString() as string,
+      status: 0
+    }
+    const response = await fetch('https://toyota-hackathon.azurewebsites.net/api/CreateTask', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(task)
+    })
+    if (response.status === 200) {
+      const newTasks = tasks === undefined ? [task] : [...tasks, task];
+      setTasks(newTasks);
+    }
+    setOpen(false)
+  }
+
   return (
     <div>
       <div className="flex justify-between items-center px-4 py-2 border-b-2 border-gray-200">
@@ -16,74 +71,68 @@ function TaskList() {
           <span className="font-bold text-sm text-blue-500">貢献度</span>
         </div>
         <div className="flex flex-col items-center">
-          <AddCircleOutlineIcon fontSize='large' className='text-blue-500 font-bold' />
+          <AddCircleOutlineIcon fontSize='large' className='text-blue-500 font-bold' onClick={() => TaskCreationClicked()} />
           <span className="font-bold text-sm text-blue-500">タスクを作成</span>
         </div>
       </div>
-      {data.map((item) => {
+      {tasks !== undefined && tasks.map((task) => {
         return (
-          <TaskItem item={item as item} />
+          <TaskItem key={task.id} item={task as item} />
         )
       })}
-    </div >
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <div className="flex items-center justify-center h-full">
+          <div className="relative w-full sm:w-1/2 bg-white rounded-md shadow-lg p-8">
+            <div style={{ position: 'absolute', top: '10px', right: '10px' }}>
+              <IconButton
+                aria-label="close"
+                onClick={handleClose}
+                className="absolute top-1 right-1"
+              >
+                <CloseIcon />
+              </IconButton>
+            </div>
+            <div className="flex flex-col mt-10">
+              <Typography>タイトル</Typography>
+              <OutlinedInput
+                id="outlined-adornment-weight"
+                size="small"
+                aria-describedby="outlined-weight-helper-text"
+                sx={{ mb: 2 }}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+              <Typography>説明</Typography>
+              <TextField
+                id="outlined-textarea"
+                aria-describedby="outlined-weight-helper-text"
+                multiline
+                rows={3}
+                variant="outlined"
+                sx={{ mb: 2 }}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+              <Typography>期限</Typography>
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DatePicker']}>
+                  <DatePicker
+                    sx={{ mb: 2 }}
+                    value={timeValue}
+                    onChange={(newValue) => setTimeValue(newValue)}
+                  />
+                </DemoContainer>
+              </LocalizationProvider>
+              <Button variant="contained" onClick={() => createTask()}>タスクを追加</Button>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </div>
   );
 }
 
 export default TaskList;
-
-const data = [
-  {
-    name: '鈴木',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 8',
-    status: 0,
-  },
-  {
-    name: '伊藤',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 6',
-    status: 2,
-  },
-  {
-    name: '吉田',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 4',
-    status: 1,
-  },
-  {
-    name: '高橋',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 1',
-    status: 4,
-  },
-  {
-    name: '高橋',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 1',
-    status: 3,
-  },
-  {
-    name: '高橋',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 1',
-    status: 2,
-  },
-  {
-    name: '高橋',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 1',
-    status: 2,
-  },
-  {
-    name: '高橋',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 1',
-    status: 2,
-  },
-  {
-    name: '高橋',
-    dueDate: '2023-03-05',
-    assignedTo: 'User 1',
-    status: 2,
-  },
-];
