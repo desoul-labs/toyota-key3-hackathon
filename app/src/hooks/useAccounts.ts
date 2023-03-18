e54bcb078208ba6290347b704442abb7227b8b5b
Lmmmmm <lm839377386@gmail.com>
2023-03-18 18:24:20 +0900

    fix: keyring

--- app/src/hooks/useAccounts.ts
+++ app/src/hooks/useAccounts.ts
@@ -1,17 +1,15 @@
 import { Keyring } from '@polkadot/api';
 import { KeyringPair } from '@polkadot/keyring/types';
-import { ed25519PairFromSeed } from '@polkadot/util-crypto';
-import { mnemonicGenerate, mnemonicToMiniSecret, mnemonicValidate } from '@polkadot/util-crypto/mnemonic';
-import { useMemo } from 'react';
+import { mnemonicGenerate } from '@polkadot/util-crypto/mnemonic';
 
 export function useAccount(): KeyringPair {
-  if (localStorage.getItem('account') === null || localStorage.getItem('account') === undefined) {
-    const mnemonic = mnemonicGenerate();
-    const keyring = new Keyring({ type: 'sr25519' });
-    const acc = keyring.addFromUri(mnemonic, { name: 'Default' });
-    localStorage.setItem('account', JSON.stringify(acc));
-    console.log(keyring.encodeAddress(acc.publicKey));
-    return acc;
+  let mnemonic = localStorage.getItem('mnemonic');
+  if (mnemonic === null || mnemonic === undefined) {
+    mnemonic = mnemonicGenerate();
+    localStorage.setItem('mnemonic', mnemonic);
   }
-  return JSON.parse(localStorage.getItem('account')!) as KeyringPair;
+  const keyring = new Keyring({ type: 'sr25519' });
+  const acc = keyring.addFromUri(`${mnemonic}//Alice`, { name: 'Default' });
+  console.log(keyring.encodeAddress(acc.publicKey));
+  return acc;
 }
